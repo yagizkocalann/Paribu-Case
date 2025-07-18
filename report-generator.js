5ced64be5abcbf7162d76717bbf9b1f60ce2f165
yagizkocalann <yagizkocalan@gmail.com>
2025-07-18 19:55:15 +0300

    first_commit
    first commit

--- report-generator.js
+++ report-generator.js
@@ -1,7 +1,6 @@
 const report = require('multiple-cucumber-html-reporter');
 
-// We install npm-run-all to run tests in parallel for different browsers.
-// Using a shared output directory ('reports') for all browsers simplifies report generation by consolidating all test results in one place.
+
 report.generate({
   jsonDir: './reports',
   reportPath: './reports/html',
